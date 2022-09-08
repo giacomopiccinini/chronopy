4b7e3ea5e788772707b366397127db71adba755c
Giacomo Piccinini <piccinini.giacomo@gmail.com>
2022-09-08 14:56:34 +0200

    [FEAT] Added possibility of assessing performance of function as an iterable varies

--- Clock.py
+++ Clock.py
@@ -28,6 +28,23 @@ class Clock():
         # Store row number where the clock is called
         self.rows.append(currentframe().f_back.f_lineno)
 
+
+    def timings(self, initial_block, final_block):
+
+        """ Compute timings for various blocks """
+
+        # Ensure they are correctly ordered
+        if initial_block > final_block:
+            print("Error: fix block number!")
+            return
+
+        # Compute timings
+        block_timings = [self.laps[n] - self.laps[n-1] for n in range(initial_block, final_block + 1)]
+
+        return block_timings
+
+
+
     def summary(self):
 
         # Computer number of blocks
@@ -42,4 +59,34 @@ class Clock():
             print(f"Block {n + 1}: {self.laps[n + 1] - self.laps[n]}; Rows {self.rows[n]} to {self.rows[n + 1]}")
 
 
+    def evaluate(self, function, args, iterable, iterable_name):
+
+        """ Evaluate how function scales with varying iterable"""
+
+        # Initialise block number
+        if self.n_laps == 0:
+            initial_block = 1
+        else:
+            initial_block = self.n_laps - 1
+
+        # Compute number of final block
+        final_block   = initial_block + len(iterable) - 1
+
+
+        for it in iterable:
+
+            # Update args dictionary
+            args[iterable_name] = it
+            # Start clocl
+            self.lap()
+            # Evaluate function
+            function(**args)
+
+        # Stop fime
+        self.lap()
+
+        # Compute timings
+        timings = self.timings(initial_block, final_block)
+
+        return timings
 
