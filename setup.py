2a7660d00bb22b7725456d43cd9717e28f8ebc45
Giacomo Piccinini <piccinini.giacomo@gmail.com>
2022-09-12 12:41:05 +0200

    [FIX] Fix names

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='chronopy',
+    version='0.1.0',    
+    description='A simple Python package to measure execution time',
+    url='https://github.com/shuds13/pyexample',
+    author='Stephen Hudson',
+    author_email='shudson@anl.gov',
+    license='BSD 2-clause',
+    packages=['pyexample'],
+    install_requires=['time',
+                      'inspect',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
